5120bf6a76d6baa74ca3cfa669edf0ed6f95622f
Timur Mingulov <timming@timming-osx2.local>
2017-11-05 16:18:55 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# rzd_parser
-parse rzd.ru to catch train tickets
+## Description
+Parse rzd.ru to catch train tickets
+
+This tool parses pass.rzd.ru site every 10 minutes and notifies user in Telegram, if a ticket occurs
+
+## Usage
+### Preparation
+In order to use this tool you need to take some preparation steps:
+0. Install Mozilla Firefox browser
+1. Install selenium webdriver
+`pip install selenium`
+2. Install request library
+`pip install requests`
+3. Create Telegram bot and get token
+
+### Tool
+1. `git clone https://github.com/mingghan/rzd_parser.git`
+2. Put your Telegram bot token into `config.py`
+3. Run `python parse_rzd <url> <ticket type to find>`
+For example, `python parse_rzd.py "https://pass.rzd.ru/tickets/public/ru?STRUCTURE_ID=704&refererPageId=4819&layer_name=e3-route&tfl=3&st0=МОСКВА&code0=2000000&dt0=05.11.2017&st1=ЯРОСЛАВЛЬ&code1=2010000&checkSeats=1" "Сидячий"`
+4. Script is going to parse the page to find specified ticket every 10 minutes and notify you in Telegram
---
+++ parse_rzd.py
@@ -0,0 +1,116 @@
+#!/usr/bin/python2.7
+#-*- coding:utf-8 -*-
+
+from selenium import webdriver
+import time
+import re
+import datetime
+import sys
+import config
+import json
+import requests
+import urllib
+
+def parse_rzd(counter, url, text_to_find):
+	page_url = url
+
+	browser = webdriver.Firefox()
+	page = browser.get(page_url)
+
+	time.sleep(10)
+	seats = browser.find_elements_by_xpath("//*[contains(text(), {0})]".format(text_to_find))
+
+	if len(seats)>0:
+		print "There is tickets there"
+		return "There is tickets there"
+
+	print  "%d. No tickets yet" % counter
+	browser.close()
+
+TOKEN = config.token
+URL = "https://api.telegram.org/bot{}/".format(TOKEN)
+
+
+def get_url(url):
+	response = requests.get(url)
+	content = response.content.decode("utf8")
+	return content
+
+
+def get_json_from_url(url):
+	content = get_url(url)
+	js = json.loads(content)
+	return js
+
+
+def get_updates(offset=None):
+	url = URL + "getUpdates"
+	if offset:
+		url += "?offset={}".format(offset)
+	js = get_json_from_url(url)
+	return js
+
+
+def get_last_update_id(updates):
+	update_ids = []
+	for update in updates["result"]:
+		update_ids.append(int(update["update_id"]))
+	return max(update_ids)
+
+
+def echo_all(updates):
+	for update in updates["result"]:
+		text = update["message"]["text"]
+		chat = update["message"]["chat"]["id"]
+		send_message(text, chat)
+
+
+def get_last_chat_id_and_text(updates):
+	num_updates = len(updates["result"])
+	last_update = num_updates - 1
+	text = updates["result"][last_update]["message"]["text"]
+	chat_id = updates["result"][last_update]["message"]["chat"]["id"]
+	return (text, chat_id)
+
+def get_last_chat_id(updates):
+	num_updates = len(updates["result"])
+	last_update = num_updates - 1
+	chat_id = updates["result"][last_update]["message"]["chat"]["id"]
+	return chat_id
+
+
+def send_message(text, chat_id):
+	text = urllib.quote_plus(text)
+	url = URL + "sendMessage?text={}&chat_id={}".format(text, chat_id)
+	get_url(url)
+
+def main():
+	last_update_id = None
+	chat_id = None
+	counter = 0
+	url = ""
+	text_to_find = ""
+	if len(sys.argv) > 2:
+		url = sys.argv[1]
+		text_to_find = sys.argv[2]
+	else:
+		print "Please provide the url and text to find"
+		return
+	while True:
+		time.sleep(5)
+		if chat_id is None:
+			updates = get_updates(last_update_id)
+			chat_id =  get_last_chat_id(updates)
+		text = ''
+		counter += 1
+		try:
+			text = parse_rzd(counter, url, text_to_find)
+		except Exception, e:
+			print e
+
+		if text and chat_id:
+			send_message(text, chat_id)
+		time.sleep(10*60)
+
+if __name__ == '__main__':
+	main()
